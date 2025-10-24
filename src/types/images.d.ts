d39f47554c9e918175a9906bf0c3f638bf42f938
Lins <matheushlm2@gmail.com>
2025-10-24 10:52:47 -0300

    Correcao erros na vercel

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,20 +1,24 @@
 declare module '*.png' {
-    const value: string;
-    export default value;
-  }
-  
-  declare module '*.jpg' {
-    const value: string;
-    export default value;
-  }
-  
-  declare module '*.jpeg' {
-    const value: string;
-    export default value;
-  }
-  
-  declare module '*.svg' {
-    const value: string;
-    export default value;
-  }
-  
+  const value: import('next/image').StaticImageData;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: import('next/image').StaticImageData;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: import('next/image').StaticImageData;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: import('next/image').StaticImageData;
+  export default value;
+}
+
+declare module '*.ico' {
+  const value: import('next/image').StaticImageData;
+  export default value;
+}
